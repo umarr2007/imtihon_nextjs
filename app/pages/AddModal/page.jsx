51b0ea7fc2fb455213d34257b4d822c245bd29c9
umarr2007 <umarjon2007@gmail.com>
2025-05-08 17:45:01 +0500

    Update page.jsx

--- app/pages/AddModal/page.jsx
+++ app/pages/AddModal/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { Modal, Box, TextField, Button, Typography } from "@mui/material";
 import { ToastContainer, toast } from "react-toastify";
